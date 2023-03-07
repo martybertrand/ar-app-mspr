ddc5c1013e92bcffd1fb061b01f169c18ef13ee3
martybertrand <marty.bertrand@hotmail.com>
2023-03-07 15:21:29 +0100

    test AR

--- src/App.js
+++ src/App.js
@@ -1,28 +1,47 @@
-import { render } from 'react-dom';
-import React, { useRef } from 'react';
-import './App.css'
-import { ZapparCamera, ImageTracker, ZapparCanvas, BrowserCompatibility } from '@zappar/zappar-react-three-fiber';
 
-export default function App() {
-  // Setup a camera ref, as we need to pass it to the tracker.
-  const camera = useRef();
-  // Use Webpack to load in target file
-  const targetFile = require('./Logo.png').default;
+
+
+
+
+import React, { useState } from 'react';
+import {
+ ZapparCamera, InstantTracker, ZapparCanvas, BrowserCompatibility,
+} from '@zappar/zappar-react-three-fiber';
+
+function App() {
+  const [placementMode, setPlacementMode] = useState(true);
   return (
-    <><BrowserCompatibility /><ZapparCanvas>
-      {/* Setup Zappar Camera, setting camera object's ref */}
-      <ZapparCamera ref={camera} />
-      {/* Setup Image Tracker, passing our target file and camera ref */}
-      <ImageTracker targetImage={targetFile} camera={camera}>
-        {/* Create a normal pink sphere to be tracked to the target */}
-        <mesh>
-          <sphereBufferGeometry />
-          <meshStandardMaterial color="hotpink" />
-        </mesh>
-      </ImageTracker>
-      {/* Normal directional light */}
-      <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
-    </ZapparCanvas></>
+    <>
+      <BrowserCompatibility />
+      <ZapparCanvas>
+        <ZapparCamera />
+        <InstantTracker placementMode={placementMode} placementCameraOffset={[0, 0, -5]}>
+          <mesh>
+            <boxBufferGeometry />
+            <meshStandardMaterial color="hotpink" />
+          </mesh>
+        </InstantTracker>
+        <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
+
+      </ZapparCanvas>
+      <div
+        id="zappar-button"
+        role="button"
+        onKeyPress={() => { setPlacementMode(((currentPlacementMode) => !currentPlacementMode)); }}
+        tabIndex={0}
+        onClick={() => { setPlacementMode(((currentPlacementMode) => !currentPlacementMode)); }}
+      >
+        Tap here to
+        {placementMode ? ' place ' : ' pick up '}
+        the object
+      </div>
+    </>
   );
 }
-render(<App />, document.getElementById('root'));
+
+export default App;
+
+
+
+
+
