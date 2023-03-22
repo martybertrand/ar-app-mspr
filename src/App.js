81af1d44a6ee3fce2f993123b0c22959ba7bb02a
martybertrand <marty.bertrand@hotmail.com>
2023-03-22 19:58:22 +0100

    test with model 3D

--- src/App.js
+++ src/App.js
@@ -1,58 +1,41 @@
-import { ZapparCamera, ZapparCanvas } from "@zappar/zappar-react-three-fiber";
-import { useLoader } from "@react-three/fiber";
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-
-import React from 'react'
-
+import React, { useState } from 'react';
+import { ZapparCamera, InstantTracker, ZapparCanvas, BrowserCompatibility } from '@zappar/zappar-react-three-fiber';
+import { useLoader } from '@react-three/fiber';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
 
 export default function App() {
-  
-  const gltf = useLoader(GLTFLoader, './model/model.gltf');
-
-    return (
-      <ZapparCanvas>
-        <ZapparCamera>
-        <primitive object={gltf.scene} />
-      </ZapparCamera>
-      </ZapparCanvas>
-      
-    )
-}
-
-
-/*import React, { useState } from 'react';
-import {
- ZapparCamera, InstantTracker, ZapparCanvas, BrowserCompatibility, ImageTracker
-} from '@zappar/zappar-react-three-fiber';
-import MyCustomObject from './MyCustomObject';
-
-function App() {
   const [placementMode, setPlacementMode] = useState(true);
+  const model = useLoader(GLTFLoader, '/model/test.gltf');
+
   return (
-    <>
-      <BrowserCompatibility />
-      <ZapparCanvas>
-        <ZapparCamera />
-        
-        <ImageTracker placementMode={placementMode} placementCameraOffset={[0, 0, -5]}>
-            <MyCustomObject/>
-        </ImageTracker>
-      <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
+  <>
+       <BrowserCompatibility />
+       <ZapparCanvas>
+         <ZapparCamera />
+         <InstantTracker placementMode={placementMode} placementCameraOffset={[0, 0, 0]}>
+           <primitive object={model.scene} />
+         </InstantTracker>
+         <directionalLight position={[2.5, 8, 5]} intensity={1.5} />
 
-      </ZapparCanvas>
-      <div
-        id="zappar-button"
-        role="button"
-        onKeyPress={() => { setPlacementMode(((currentPlacementMode) => !currentPlacementMode)); }}
-        tabIndex={0}
-        onClick={() => { setPlacementMode(((currentPlacementMode) => !currentPlacementMode)); }}
-      >
-        Tap here to
-        {placementMode ? ' place ' : ' pick up '}
-        the object
-      </div>
-    </>
-  );
-}
+       </ZapparCanvas>
+       <div
+         id="zappar-button"
+         role="button"
+         onKeyPress={() => { setPlacementMode(((currentPlacementMode) => !currentPlacementMode)); }}
+         tabIndex={0}
+         onClick={() => { setPlacementMode(((currentPlacementMode) => !currentPlacementMode)); }}
+       >
+         Tap here to
+         {placementMode ? ' place ' : ' pick up '}
+         the object
+       </div>
+     </>
+   );
+ }
 
-export default App;*/
+ /*
+<mesh>
+             <boxBufferGeometry />
+             <meshStandardMaterial color="hotpink" />
+           </mesh>
+ */
